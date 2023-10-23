839983d74d8db14e1dfc5c79582797c0be5fc536
AbdullaArif <arifabdulla034@gmail.com>
2023-10-24 00:07:21 +0400

    Task EEP 2

---
+++ LuhinAlgoritm/LuhinAlgoritm/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace LuhinAlgoritm
+{
+   using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a card number (without spaces):");
+        string cardNumber = Console.ReadLine();
+
+        if (IsLuhnValid(cardNumber))
+        {
+            Console.WriteLine("The card number is valid.");
+        }
+        else
+        {
+            Console.WriteLine("The card number is not valid.");
+        }
+    }
+
+    static bool IsLuhnValid(string cardNumber)
+    {
+        int total = 0;
+        bool doubleDigit = false;
+
+        for (int i = cardNumber.Length - 1; i >= 0; i--)
+        {
+            int digit = cardNumber[i] - '0';
+
+            if (doubleDigit)
+            {
+                digit *= 2;
+                if (digit > 9)
+                {
+                    digit -= 9;
+                }
+            }
+
+            total += digit;
+            doubleDigit = !doubleDigit;
+        }
+
+        return (total % 10 == 0);
+    }
+}
+
+}
